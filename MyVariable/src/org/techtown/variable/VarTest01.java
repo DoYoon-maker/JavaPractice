12a64f54d820256c8a737f7417284e979b342ebd
DoYoon <dyk6208@gmail.com>
2021-08-18 22:09:06 +0900

    first commit

---
+++ MyVariable/src/org/techtown/variable/VarTest01.java
@@ -0,0 +1,19 @@
+package org.techtown.variable;
+
+public class VarTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 10;
+		
+		System.out.println(count);
+		count += 10;
+		System.out.println(count);
+		
+		String hello = "¾È³ç!";
+		System.out.println(hello);
+		
+		System.out.println(hello);
+	}
+
+}
---
+++ Study01/src/org/techtown/study01/Friends.java
@@ -0,0 +1,14 @@
+package org.techtown.study01;
+
+public class Friends {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1. ±èÇö¼ö");
+		System.out.println("2. ±èÅÂÈñ");
+		System.out.println("3. ¹Ú¸í±æ");
+		System.out.println("4. ÀÌÇÏ´Ã");
+		System.out.println("5. ºÀÁØÈ£");
+	}
+
+}
